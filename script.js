0996f13e7646d815818340e7cc25e9860341dabe
Colt15 <billyeveryman19@gmail.com>
2016-08-11 16:59:10 -0400

    adsad

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-var i;
-
-
-for( i = 1; i <= 100; i++){
+var num = prompt ('select a number', '1-100');
+function Fizzbuzz (number) { 
+    for(var i = 1; i <= number; i++){
 
 
     if (  i % 15 == 0) {
-    	document.write('Fizz Buzz<br/>');
-      	console.log('Fizz Buzz');
+        document.write('Fizz Buzz<br/>');
+        console.log('Fizz Buzz');
 
     }else if (i % 5 == 0) {
-    	document.write('Buzz<br/>');
+        document.write('Buzz<br/>');
         console.log('buzz');
 
     }else if (i % 3 == 0) {
 
 
-		document.write('Fizz<br/>');
+        document.write('Fizz<br/>');
         console.log('Fizz');
 
 
     }else {
 
-    	document.write(i + '<br/>');
+        document.write(i + '<br/>');
         console.log(i);
     }
 
 
 
 }
-
+}
+Fizzbuzz(num);
